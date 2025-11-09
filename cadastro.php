975c464b14a529def22c5a55bcc5b634356790e4
Jlocked1 <jeancarneiro437@gmail.com>
2025-11-09 20:52:33 +0000

    Atualizações na página dashboard

---
+++ cadastro.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$telefone = $_POST["telefone"];
+$senha = $_POST['senha'];
+
+if($nome != NULL && $email != NULL && $telefone != NULL && $senha != NULL) {
+    echo 'Dados Recebidos. Redirecionando...';
+    sleep(2.25);
+    header( 'Location: http://localhost/EstacioSa/auth.html' );
+}
+
+
---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+if($email != NULL && $senha != NULL ) {
+    echo 'Login com sucesso. Redirecionando...';
+    sleep(2.25);
+    header( 'Location: /dashboard.html' );
+}
